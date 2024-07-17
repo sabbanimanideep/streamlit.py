5706e492a0d7545e5f848196c058fd9c6f7fe070
sabbanimanideep <mani06271@gmail.com>
2024-07-17 01:49:31 -0700

    Add files via upload

--- streamlit.py
+++ streamlit.py
@@ -1,1 +1,9 @@
+import streamlit as st
+
+st.title('just for learn')
+st.subheader('login in below ')
+st.text_input('Enter your email addhers ')
+st.text_input('Enter your password ')
+st.button('Login')
+st.radio('select your gender',["male",'female','other'])
 
